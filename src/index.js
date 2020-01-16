1aa6a076eb4912f14f1606abcf28b2f96d9af1d5
Pedro <pedrofellipeunai@gmail.com>
2020-01-16 16:21:31 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -10,8 +11,9 @@ mongoose.connect('mongodb+srv://omnistack10:omnistack10@cluster0-jznev.mongodb.n
   useUnifiedTopology: true
 });
 
-app.use(express.json());
 
+app.use(cors({ origin: 'http://localhost:3000'}));
+app.use(express.json());
 app.use(routes);
 
 
